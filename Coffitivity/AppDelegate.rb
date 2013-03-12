721ff65460ccaaa81d2da71426d2e96158dc2074
Mick Staugaard <mick@staugaard.com>
2013-03-12 09:54:53 -0700

    whitespace

--- Coffitivity/AppDelegate.rb
+++ Coffitivity/AppDelegate.rb
@@ -1,71 +1,62 @@
-#
-#  AppDelegate.rb
-#  Coffitivity
-#
-#  Created by Mick Staugaard on 3/11/13.
-#  Copyright 2013 Mick Staugaard. All rights reserved.
-#
-
 class AppDelegate
-    attr_accessor :imageView
-    attr_accessor :logoMenuItem
+  attr_accessor :imageView
+  attr_accessor :logoMenuItem
 
-    attr_accessor :volumeSlider
-    
-    attr_accessor :statusMenu
-    attr_accessor :statusItem
+  attr_accessor :volumeSlider
 
-    def applicationDidFinishLaunching(a_notification)
-        # Insert code here to initialize your application
-    end
+  attr_accessor :statusMenu
+  attr_accessor :statusItem
 
-    def awakeFromNib
-        imageView.image = image('http://coffitivity.com/images/coff_logobanner.png', [ 200, 51 ])
-        
-        @statusItem = NSStatusBar.systemStatusBar.statusItemWithLength(NSVariableStatusItemLength)
-        @statusItem.menu = statusMenu
-        @statusItem.image = image('http://coffitivity.com/images/favicon.ico', [ 17, 17 ])
-        @statusItem.highlightMode = true
+  def applicationDidFinishLaunching(a_notification)
+    # Insert code here to initialize your application
+  end
 
-        volumeSlider.floatValue = volume
-    end
+  def awakeFromNib
+    imageView.image = image('http://coffitivity.com/images/coff_logobanner.png', [ 200, 51 ])
 
-    def sound
-        @sound ||= NSSound.alloc.initWithContentsOfURL(NSURL.alloc.initWithString('http://coffitivity.com/other/audio3.mp3'), byReference: true).tap do |s|
-            s.loops = true
-            s.volume = volume
-        end
-    end
+    @statusItem = NSStatusBar.systemStatusBar.statusItemWithLength(NSVariableStatusItemLength)
+    @statusItem.menu = statusMenu
+    @statusItem.image = image('http://coffitivity.com/images/favicon.ico', [ 17, 17 ])
+    @statusItem.highlightMode = true
 
-    def volume
-       NSUserDefaults.standardUserDefaults.floatForKey('volume') || 0.65
-    end
+    volumeSlider.floatValue = volume
+  end
 
-    def volume=(value)
-        NSUserDefaults.standardUserDefaults.setFloat(value, forKey: 'volume')
-        NSUserDefaults.standardUserDefaults.synchronize
+  def sound
+    @sound ||= NSSound.alloc.initWithContentsOfURL(NSURL.alloc.initWithString('http://coffitivity.com/other/audio3.mp3'), byReference: true).tap do |s|
+      s.loops = true
+      s.volume = volume
     end
-    
-    def toggleProductivity(sender)
-        if sender.state == NSOnState
-            sound.play
-        else
-            sound.stop
-        end
-    end
+  end
 
-    def changeVolume(sender)
-        sound.volume = sender.floatValue
-        self.volume = sender.floatValue
+  def volume
+    NSUserDefaults.standardUserDefaults.floatForKey('volume') || 0.65
+  end
+
+  def volume=(value)
+    NSUserDefaults.standardUserDefaults.setFloat(value, forKey: 'volume')
+    NSUserDefaults.standardUserDefaults.synchronize
+  end
+
+  def toggleProductivity(sender)
+    if sender.state == NSOnState
+      sound.play
+    else
+      sound.stop
     end
-    
-    private
-    
-    def image(url, size)
-        the_url = NSURL.alloc.initWithString(url)
-        img = NSImage.alloc.initByReferencingURL(the_url)
-        img.size = size
-        img
-    end
+  end
+
+  def changeVolume(sender)
+    sound.volume = sender.floatValue
+    self.volume = sender.floatValue
+  end
+
+  private
+
+  def image(url, size)
+    the_url = NSURL.alloc.initWithString(url)
+    img = NSImage.alloc.initByReferencingURL(the_url)
+    img.size = size
+    img
+  end
 end
-
